5da4c84b1bfc5a675f77b836f60a34dbe17f353b
saiful-47 <saifulcse47@gmail.com>
2025-09-18 22:35:04 +0100

    project sturcture is ready

---
+++ app/config/config.js
@@ -0,0 +1,18 @@
+export const PORT = 5000;
+export const DATABASE = 'DATABASE';
+export const JWT_KEY = 'JWT_KEY';
+export const JWT_SECRET = 'JWT_SECRET';
+export const JWT_EXPIRATION = 1000;
+
+export const EMAIL_HOST = 'EMAIL_HOST';
+export const EMAIL_PASSWORD = 'EMAIL_PASSWORD';
+export const EMAIL_SECURITY = 'EMAIL_SECURITY';
+export const EMAIL_PORT = 25;
+export const EMAIL_UN_AUTH = false;
+
+export const WEB_CACHE = false;
+export const MAX_JSON_SIZE = "10M";
+export const URL_ENCODING = true;
+
+export const REQUEST_TIMEOUT = 20*60*1000;
+export const REQUEST_NUMBER =2000;
