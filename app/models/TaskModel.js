66d5ab4f51bddec34b915f695b4d25ebba9eadd5
saiful-47 <saifulcse47@gmail.com>
2025-09-22 00:26:17 +0100

    tasks model schema done

---
+++ app/models/TaskModel.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const TaskSchema = new mongoose.Schema({
+    name:{type:String, required: true},
+    description:{ type: String, required: true },
+    status:{type:String, required: true},
+    user_id:{type:mongoose.Schema.Types.ObjectId, required: true}
+
+},{
+    timestamps: true,
+    versionKey: false
+})
+
+const Tasks = mongoose.model('Tasks', TaskSchema);
+
+export default Tasks;
+
